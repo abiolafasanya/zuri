72bc58dd2f1c15e726fd65f8f516088ba18cc034
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-04-27 22:53:17 +0100

    added index for heroku

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Zuri Internship Task</title>
+</head>
+<body>
+    <div class="container">
+    <div class="jumbotron text-center">
+        <h1 class="text-info">Welcome to my Zuri Task Dashboard</h1>
+        <p class="">
+            Have a nice time checking and lemme know where
+            i have missed it, if you see errors or correction
+        </p>
+          <a href="contact" class="btn btn-success">Message me</a>
+    </div>
+        <div class="row justify-content-center m-5">
+            <ul class="list-group">
+                <li class="list-group-item">Authentication Using File System in PHP
+                    <a class="btn btn-info float-right" href="task1">View</a>
+                </li>
+                
+                <li class="list-group-item">PHP Crud System &nbsp;
+                    <a class="btn btn-info float-right" href="task2">View</a>
+                </li>
+            </ul>
+        </div>
+        <footer>
+            <div class="text-center">&copy; Abiola Fasanya @Github->fastbeetech Student Id: ZUR006015N57</div>
+        </footer>
+    </div>
+</body>
+</html>
