99a6b3359ce3758f9a274d0b47936d99e705dcfb
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-04-27 23:03:31 +0100

    bootstrap js

--- task2/layouts/header.php
+++ task2/layouts/header.php
@@ -17,6 +17,11 @@
     <!-- bootstrap css -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <!-- bootstrap js -->
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.min.js"></script>
+
     <title>Index page</title>
 </head>
 <body>
