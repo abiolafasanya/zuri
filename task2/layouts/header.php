18a38d21407470d3df150211251318b66c1577e5
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-04-30 13:30:24 +0100

    updated folder

--- task2/layouts/header.php
+++ task2/layouts/header.php
@@ -15,13 +15,13 @@
     rel="stylesheet"
     />
     <!-- bootstrap css -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
     <!-- bootstrap js -->
-    <script src="../js/jquery.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
     <title>Index page</title>
 </head>
 <body>
