be841daa8d99c8a2faa423fe7a0c0dc190697a4f
Fasanya Abiola <79338077+fastbeetech@users.noreply.github.com>
2021-05-15 16:52:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
                     <a class="btn btn-info float-right" href="task1">View</a>
                 </li>
                 
-                <li class="list-group-item">PHP Crud System &nbsp;
+                <li class="list-group-item">PHP Course dashboard &nbsp;
                     <a class="btn btn-info float-right" href="task2">View</a>
                 </li>
             </ul>
